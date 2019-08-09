42dff69c6a40938895ad8be028f2edf00d4d3ca6
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2019-08-09 16:19:38 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,27 @@
 <?php
 
+$contents = $_FILES['image'];
+$file = 'php://memory'; //full memory buffering mode
+//$file = 'php://temp/maxmemory:1048576'; //partial memory buffering mode. Tries to use memory, but over 1MB will automatically page the excess to a file.
+$o = new SplFileObject($file, 'w+');
+$o->fwrite($contents);
+
 // Step 1 - Scan for viruses with Cloudmersive Virus Scan
 
+require_once(__DIR__ . '/vendor/autoload.php');
 
+// Configure API key authorization: Apikey
+$config = Swagger\Client\Configuration::getDefaultConfiguration()->setApiKey('Apikey', 'YOUR_API_KEY');
+
+
+
+$apiInstance = new Swagger\Client\Api\ScanApi(
+    new GuzzleHttp\Client(),
+    $config
+);
+$input_file = $o; // \SplFileObject | Input file to perform the operation on.
+
+$result = $apiInstance->scanFile($input_file);
 
 // Step 2 - Upload to S3
 
