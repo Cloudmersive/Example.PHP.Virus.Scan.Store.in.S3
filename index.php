6e3d02fcf98eef302b0f9a847b83dc91ccaf1261
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2019-08-09 16:42:44 -0700

    Added

--- index.php
+++ index.php
@@ -1,35 +1,49 @@
 <?php
 
-$contents = $_FILES['image'];
-$file = 'php://memory'; //full memory buffering mode
-//$file = 'php://temp/maxmemory:1048576'; //partial memory buffering mode. Tries to use memory, but over 1MB will automatically page the excess to a file.
-$o = new SplFileObject($file, 'w+');
-$o->fwrite($contents);
+if(isset($_FILES['image']))
+{
+
+$file_name = $_FILES['image']['name'];   
+$temp_file_location = $_FILES['image']['tmp_name']; 
+
+//$contents = $_FILES['image'];
+// $file = 'php://memory'; //full memory buffering mode
+// //$file = 'php://temp/maxmemory:1048576'; //partial memory buffering mode. Tries to use memory, but over 1MB will automatically page the excess to a file.
+// $o = new SplFileObject($file, 'w+');
+// $o->fwrite($contents);
+
 
 // Step 1 - Scan for viruses with Cloudmersive Virus Scan
 
 require_once(__DIR__ . '/vendor/autoload.php');
 
 // Configure API key authorization: Apikey
-$config = Swagger\Client\Configuration::getDefaultConfiguration()->setApiKey('Apikey', 'YOUR_API_KEY');
+$config = Swagger\Client\Configuration::getDefaultConfiguration()->setApiKey('Apikey', 'API-KEY-GOES-HERE');
 
 
 
 $apiInstance = new Swagger\Client\Api\ScanApi(
-    new GuzzleHttp\Client(),
+    new GuzzleHttp\Client(  array('verify'          => false)  ),
     $config
 );
-$input_file = $o; // \SplFileObject | Input file to perform the operation on.
+$input_file = $temp_file_location; // \SplFileObject | Input file to perform the operation on.
 
 $result = $apiInstance->scanFile($input_file);
 
+if (! $result->getCleanResult())
+{
+    throw new Exception('File contains viruses!');
+}
+
+file_put_contents("php://output", "File is clean and safe to upload, proceeding to upload");
+
 // Step 2 - Upload to S3
 
-	if(isset($_FILES['image'])){
-		$file_name = $_FILES['image']['name'];   
-		$temp_file_location = $_FILES['image']['tmp_name']; 
 
-		require 'vendor/autoload.php';
+try
+{
+
+		// require 'vendor/autoload.php';
 
 		$s3 = new Aws\S3\S3Client([
 			'region'  => '-- your region --',
@@ -47,7 +61,13 @@ $result = $apiInstance->scanFile($input_file);
 		]);
 
 		var_dump($result);
-	}
+}
+catch (Exception $e)
+{
+    
+}
+
+}
 ?>
 
 <form action="<?= $_SERVER['PHP_SELF']; ?>" method="POST" enctype="multipart/form-data">         
